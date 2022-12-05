d2a010ca4f623b841e4b69d0202b7d93fb85ebb1
Konstantin-create <schahov.konst@gmail.com>
2022-12-05 07:55:00 +0300

    create base structure

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# Base imports
+import os
+
+# My modules imports
+from config import Config
+
+# Flask imports
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+
+# Flask init
+app = Flask(__file__)
+app.config.from_object(Config)
+
+# DB init
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
---
+++ main.py
@@ -0,0 +1,3 @@
+from app import *
+
+app.run(host='0.0.0.0')
