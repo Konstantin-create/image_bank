b15a5e439e6d3e66013455dc494e057b1f5dd089
Konstantin-create <schahov.konst@gmail.com>
2022-12-07 09:40:39 +0200

    styles: index -page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,4 +3,8 @@ from app import app, render_template
 
 @app.route('/')
 def index_page():
-    return render_template('index_page.html')
+    content_data = {
+        'headers': ['My web-site', 'GitHub'],
+        'header_links': ['https://hacknet-dev.tech/', 'https://github.com/Konstantin-create/']
+    }
+    return render_template('index_page.html', content_data=content_data)
