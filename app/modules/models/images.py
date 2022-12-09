a583d4418ff6dd75ae3cce3d09eb3088d9110d28
Konstantin-create <schahov.konst@gmail.com>
2022-12-09 07:54:18 +0300

    update db

--- app/modules/models/images.py
+++ app/modules/models/images.py
@@ -4,5 +4,6 @@ from datetime import datetime
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    image_path = db.Column(db.String, unique=True)
+    image_path = db.Column(db.String, unique=True, nullable=False)
+    from_ip = db.Column(db.String, nullable=False)
     time_stamp = db.Column(db.DateTime, default=datetime.utcnow())
