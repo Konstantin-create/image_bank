3d9c0a209230378eb923c93d9f13cfc1ff1f1860
Konstantin-create <schahov.konst@gmail.com>
2022-12-09 07:56:40 +0300

    upgrade structure

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,2 @@
-from app import app, render_template
-
-
-@app.route('/')
-def index_page():
-    content_data = {
-        'headers': ['My web-site', 'GitHub'],
-        'header_links': ['https://hacknet-dev.tech/', 'https://github.com/Konstantin-create/']
-    }
-    return render_template('index_page.html', content_data=content_data)
+from .pages_routes import *
+from .handlers_routes import *
---
+++ app/routes/handlers_routes.py
@@ -0,0 +1,6 @@
+from app import app, render_template
+
+
+@app.route('/add/image', methods=['GET', 'POST'])
+def add_image():
+    return
