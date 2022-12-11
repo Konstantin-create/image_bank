4be6857fe6c2d4e5f5529ed275582d6d945eeb06
Konstantin-create <schahov.konst@gmail.com>
2022-12-11 17:30:41 +0300

    feat: image router function

--- app/routes/pages_routes.py
+++ app/routes/pages_routes.py
@@ -1,4 +1,5 @@
 from app import app, render_template
+from app.modules.images_tools import *
 
 
 @app.route('/')
@@ -8,3 +9,11 @@ def index_page():
         'header_links': ['https://hacknet-dev.tech/', 'https://github.com/Konstantin-create/']
     }
     return render_template('index_page.html', content_data=content_data)
+
+
+@app.route('/image/<int:image_id>')
+def image_page(image_id: int):
+    image = get_image(image_id)
+    if not image:
+        return '404'
+    return render_template('image.html', image_id=image.id)
