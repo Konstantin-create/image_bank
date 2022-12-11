a6e5e278580162ee00e91e25eb6f52311560b0c4
Konstantin-create <schahov.konst@gmail.com>
2022-12-11 17:06:39 +0300

    feat: add image - connect to db

--- app/routes/handlers_routes.py
+++ app/routes/handlers_routes.py
@@ -1,3 +1,6 @@
+from app.modules.images_tools import *
+
+from app import db
 from app import app, render_template, request, redirect
 
 
@@ -8,3 +11,11 @@ def add_image():
         if not image:
             return redirect('/')
 
+        current_id = get_last_id() + 1
+        image.save(f'app/static/data/image-{current_id}.jpg')
+
+        image_obj = add_image(image_path=f'app/static/data/image-{current_id}.jpg', from_ip=request.remote_addr)
+        db.session.add(image_obj)
+        db.session.commit()
+        return redirect(f'/image/{current_id}')
+    return redirect('/')
