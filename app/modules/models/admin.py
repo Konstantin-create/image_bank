4cc104f0e4849af08d297c42b82e2147b9718489
Konstantin-create <schahov.konst@gmail.com>
2022-12-11 20:46:38 +0300

    add admin table

---
+++ app/modules/models/admin.py
@@ -0,0 +1,18 @@
+from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Admin(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<Admin: {self.username}>'
