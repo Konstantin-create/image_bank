404149926907a6f95c390e61167a6fbd67c1b9e6
Konstantin-create <schahov.konst@gmail.com>
2022-12-12 07:55:56 +0300

    feat: get some data

--- app/routes/admin/admin_pages.py
+++ app/routes/admin/admin_pages.py
@@ -3,6 +3,9 @@ from app import app, render_template, redirect
 from flask_login import current_user
 from app.modules.models import *
 
+from sqlalchemy import func
+from sqlalchemy.sql import label
+
 
 @app.route('/admin/login')
 def admin_login_page(error_code: int = 100):  # dev: Code 100 is OK code
@@ -11,4 +14,18 @@ def admin_login_page(error_code: int = 100):  # dev: Code 100 is OK code
 
 @app.route('/admin/dashboard')
 def admin_dashboard_page():
-    return render_template('admin/dashboard_page.html')
+    if not current_user.is_authenticated:
+        return redirect('/admin/login')
+
+    images = Image.query.all()
+    unique_ips = set()
+    for image in images:
+        unique_ips.add(image.from_ip)
+
+    data = {
+        'images': {
+            'total': len(images),
+            'unique_ip': len(unique_ips)
+        }
+    }
+    return render_template('admin/dashboard_page.html', data=data)
